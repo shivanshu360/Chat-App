f661d5ae70677a9850bcd02791249ea3b96a761b
Shivanshu Singh <shivamsinghshivam360@gmail.com>
2026-09-09 23:24:23 +0530

    signup section code refactoring

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -34,8 +34,14 @@ export const signup = async (req,res) => {
         })
 
         if(newUser){
-            generateToken(newUser._id,res)
-            await newUser.save()
+            //before code rabbit
+            // generateToken(newUser._id,res)
+            // await newUser.save()
+
+            //after code rabbit
+            //persist user first,then issue auth cookie
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id,res);
 
             res.status(201).json({
                 _id:newUser._id,
