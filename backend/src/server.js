3fb7326c00010c62821f6905b51e1b6dfeedbf84
Shivanshu Singh <shivamsinghshivam360@gmail.com>
2026-09-10 14:11:08 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -8,12 +9,14 @@ import { connectDB } from "./lib/db.js"
 import { ENV } from "./lib/env.js";
 
 
+
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());//req.body
+app.use(cookieParser)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
