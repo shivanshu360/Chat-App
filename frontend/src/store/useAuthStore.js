35a2656b66678eb92f8898d36742b5b146b07e66
Shivanshu Singh <shivamsinghshivam360@gmail.com>
2026-09-12 10:05:38 +0530

    sidebar completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -63,5 +63,16 @@ export const useAuthStore = create((set) => ({
         }
     },
 
+    updateProfile: async (data) => {
+        try {
+          const res = await axiosInstance.put("/auth/update-profile", data);
+          set({ authUser: res.data });
+          toast.success("Profile updated successfully");
+        } catch (error) {
+          console.log("Error in update profile:", error);
+          toast.error(error.response.data.message);
+        }
+    },
+
 
 }));
